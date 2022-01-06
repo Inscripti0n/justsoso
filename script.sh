45b803c46cfaec632468789866e9e6b25f2aa811
Inscripti0n <93364049+Inscripti0n@users.noreply.github.com>
2022-01-06 15:30:14 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -9,14 +9,69 @@ url1="https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll/login"
 url2="https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll/jksb"
 for((i=0;i<num;i++))
 do
-curl -d "uid=${uidarr[i]}&upw=${upwarr[i]}&smbtn=$smbtn&hh28=907" -s $url1 -o temp.txt
-udata=$(sed -n '11p' temp.txt)
-udata=${udata#*ptopid=}
-udata=${udata%\"\}\}*}
-ptopid="${udata%&*}"
-sid="${udata#*&sid=}" #登录获取ptopid和sid
-curl -d "day6=b&did=1&men6=a" -d "ptopid=$ptopid&sid=$sid" -s $url2 -o /dev/null #进入确认界面
-curl -d "@myvs.txt" -d "jingdu=113.64&weidu=34.71&ptopid=$ptopid&sid=$sid" -s $url2 -o temp.txt #打卡
-udata=$(sed -n '24,26p' temp.txt)
-echo "$udata" >> result.html
+case $i in
+0) #第一个学号
+  curl -d "uid=${uidarr[i]}&upw=${upwarr[i]}&smbtn=$smbtn&hh28=907" -s $url1 -o temp.txt
+  udata=$(sed -n '11p' temp.txt)
+  udata=${udata#*ptopid=}
+  udata=${udata%\"\}\}*}
+  ptopid="${udata%&*}"
+  sid="${udata#*&sid=}" #登录获取ptopid和sid
+  curl -d "day6=b&did=1&men6=a" -d "ptopid=$ptopid&sid=$sid" -s $url2 -o /dev/null #进入确认界面
+  curl -d "@myvs0.txt" -d "jingdu=103.14119&weidu=24.92005&ptopid=$ptopid&sid=$sid" -s $url2 -o temp.txt #打卡
+  udata=$(sed -n '24,26p' temp.txt)
+  echo "$udata" >> result.html
+  ;;
+1) #第二个学号
+  curl -d "uid=${uidarr[i]}&upw=${upwarr[i]}&smbtn=$smbtn&hh28=907" -s $url1 -o temp.txt
+  udata=$(sed -n '11p' temp.txt)
+  udata=${udata#*ptopid=}
+  udata=${udata%\"\}\}*}
+  ptopid="${udata%&*}"
+  sid="${udata#*&sid=}" #登录获取ptopid和sid
+  curl -d "day6=b&did=1&men6=a" -d "ptopid=$ptopid&sid=$sid" -s $url2 -o /dev/null #进入确认界面
+  curl -d "@myvs1.txt" -d "jingdu=115.133660&weidu=33.231200&ptopid=$ptopid&sid=$sid" -s $url2 -o temp.txt #打卡
+  udata=$(sed -n '24,26p' temp.txt)
+  echo "$udata" >> result.html
+  ;;
+2) #第三个学号
+  curl -d "uid=${uidarr[i]}&upw=${upwarr[i]}&smbtn=$smbtn&hh28=907" -s $url1 -o temp.txt
+  udata=$(sed -n '11p' temp.txt)
+  udata=${udata#*ptopid=}
+  udata=${udata%\"\}\}*}
+  ptopid="${udata%&*}"
+  sid="${udata#*&sid=}" #登录获取ptopid和sid
+  curl -d "day6=b&did=1&men6=a" -d "ptopid=$ptopid&sid=$sid" -s $url2 -o /dev/null #进入确认界面
+  curl -d "@myvs2.txt" -d "jingdu=109.93444&weidu=35.18396&ptopid=$ptopid&sid=$sid" -s $url2 -o temp.txt #打卡
+  udata=$(sed -n '24,26p' temp.txt)
+  echo "$udata" >> result.html
+  ;;
+  3) #第四个学号
+  curl -d "uid=${uidarr[i]}&upw=${upwarr[i]}&smbtn=$smbtn&hh28=907" -s $url1 -o temp.txt
+  udata=$(sed -n '11p' temp.txt)
+  udata=${udata#*ptopid=}
+  udata=${udata%\"\}\}*}
+  ptopid="${udata%&*}"
+  sid="${udata#*&sid=}" #登录获取ptopid和sid
+  curl -d "day6=b&did=1&men6=a" -d "ptopid=$ptopid&sid=$sid" -s $url2 -o /dev/null #进入确认界面
+  curl -d "@myvs3.txt" -d "jingdu=114.48&weidu=36.78&ptopid=$ptopid&sid=$sid" -s $url2 -o temp.txt #打卡
+  udata=$(sed -n '24,26p' temp.txt)
+  echo "$udata" >> result.html
+  ;;
+  4) #第五个学号
+  curl -d "uid=${uidarr[i]}&upw=${upwarr[i]}&smbtn=$smbtn&hh28=907" -s $url1 -o temp.txt
+  udata=$(sed -n '11p' temp.txt)
+  udata=${udata#*ptopid=}
+  udata=${udata%\"\}\}*}
+  ptopid="${udata%&*}"
+  sid="${udata#*&sid=}" #登录获取ptopid和sid
+  curl -d "day6=b&did=1&men6=a" -d "ptopid=$ptopid&sid=$sid" -s $url2 -o /dev/null #进入确认界面
+  curl -d "@myvs4.txt" -d "jingdu=112.151496&weidu=37.559082&ptopid=$ptopid&sid=$sid" -s $url2 -o temp.txt #打卡
+  udata=$(sed -n '24,26p' temp.txt)
+  echo "$udata" >> result.html
+  ;;
+*)
+  echo "You didn't have any parameter"
+  ;;
+esac
 done
